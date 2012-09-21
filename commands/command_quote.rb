228af30aa1c3ca5e84d3d92a14ad274dc910c360
Heitor Salazar <heitorsalazar@gmail.com>
2012-09-21 17:07:02 -0300

    Correctly setting namespace on quote command

--- commands/command_quote.rb
+++ commands/command_quote.rb
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
-class Commands::Quote
-  include Command::Commandable
+module Commands
+  class Quote
+    include Command::Commandable
 
-  guards ['cita[cç][ãa]o .+', 'quote .+']
-  parameter :type
-  parameter :entity
-  
-  handle do |m, params|
-    r = m.reply
-    r.body = "Comando citação ainda não implementado"
-    r
+    guards ['cita[cç][ãa]o .+', 'quote .+']
+    parameter :type
+    parameter :entity
+
+    handle do |m, params|
+      r = m.reply
+      r.body = "Comando citação ainda não implementado"
+      r
+    end
   end
 end
 
