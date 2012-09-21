18e73b4f2bd65a2af288cd469e745e0e09b9738b
Heitor Salazar <heitorsalazar@gmail.com>
2012-09-21 17:20:08 -0300

    Fixing namespace of fail command

--- commands/command_fail.rb
+++ commands/command_fail.rb
@@ -13,39 +13,40 @@ module Commands
       voter = Person.where(:identity => Blather::JID.new(m.from).stripped).first
       person = Person.named(params[:person][:name].downcase).first unless params[:person].nil?
 
-    if params[:reason]
-      params[:reason][:name] += params[:reason][:modifier] unless params[:reason][:modifier].blank?
-      reason = params[:reason][:name]
-    end
+      if params[:reason]
+        params[:reason][:name] += params[:reason][:modifier] unless params[:reason][:modifier].blank?
+        reason = params[:reason][:name]
+      end
 
-    if person && voter
-      if reason
-        point = Point.new
-        point.reason = reason
-        point.amount = -1
-        point.person = voter
+      if person && voter
+        if reason
+          point = Point.new
+          point.reason = reason
+          point.amount = -1
+          point.person = voter
 
-        person.points << point
-        person.score ||= 0
-        person.score -= 1
+          person.points << point
+          person.score ||= 0
+          person.score -= 1
 
-        if person.save
-          r.body = "Safadinho! #{person.name.capitalize} agora com #{person.score} pontos, por #{point.reason}"
+          if person.save
+            r.body = "Safadinho! #{person.name.capitalize} agora com #{person.score} pontos, por #{point.reason}"
+          else
+            r.body = 'Erro ao falhar pessoa.'
+          end
         else
-          r.body = 'Erro ao falhar pessoa.'
+          r.body = 'Motivo da pontuação não informado.'
         end
       else
-        r.body = 'Motivo da pontuação não informado.'
+        if voter
+          r.body = 'Pessoa não encontrada: ' + params[:person][:name]
+        else
+          r.body = 'Votador inválido: ' + params[:name]
+        end
       end
-    else
-      if voter
-        r.body = 'Pessoa não encontrada: ' + params[:person][:name]
-      else
-        r.body = 'Votador inválido: ' + params[:name]
-      end
-    end
 
-    r
+      r
+    end
   end
 end
 
--- commands/command_filme.rb
+++ commands/command_filme.rb
@@ -1,11 +1,14 @@
-class Commands::Filme
-  include Command::Commandable
+module Commands
+  class Filme
+    include Command::Commandable
 
-  guards ['cinema .+', 'filmes .+']
-  
-  handle do |message, params|
-    'Commando de gerenciamento de filmes nao implementado'
+    guards ['cinema .+', 'filmes .+']
+
+    handle do |message, params|
+      'Commando de gerenciamento de filmes nao implementado'
+    end
   end
 end
 
-Commands::Filme
+Commands::Filme.new
+
